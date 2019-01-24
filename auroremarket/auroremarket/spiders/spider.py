87dc942822417bbbb31d6e201118da17b3db75ba
arturzangiev <zangiev@gmail.com>
2019-01-24 13:23:58 +0000

    Fixes start_urls and next page

--- auroremarket/auroremarket/spiders/spider.py
+++ auroremarket/auroremarket/spiders/spider.py
@@ -6,66 +6,7 @@ class SpiderSpider(scrapy.Spider):
     name = 'spider'
     allowed_domains = ['auroremarket.fr']
     start_urls = [
-        'https://auroremarket.fr/s-1/categories_2-pates_riz_graines_et_cereales',
-        'https://auroremarket.fr/s-1/categories_2-the',
-        'https://auroremarket.fr/s-1/categories_2-pain_et_tartine',
-        'https://auroremarket.fr/s-1/categories_2-produits_aperitifs',
-        'https://auroremarket.fr/s-1/categories_2-legumes_au_naturel',
-        'https://auroremarket.fr/s-1/categories_2-proteines_vegetales',
-        'https://auroremarket.fr/s-1/categories_2-infusion',
-        'https://auroremarket.fr/s-1/categories_2-puree_d_oleagineux',
-        'https://auroremarket.fr/s-1/categories_2-super_aliments',
-        'https://auroremarket.fr/s-1/categories_2-fruits_secs_et_seches',
-        'https://auroremarket.fr/s-1/categories_2-gros_conditionnements',
-        'https://auroremarket.fr/s-1/categories_2-graines_et_cereales',
-        'https://auroremarket.fr/s-1/categories_2-cafe_et_cacao',
-        'https://auroremarket.fr/s-1/categories_2-contenants_pour_vrac',
-        'https://auroremarket.fr/s-1/categories_2-sauces_et_condiments',
-        'https://auroremarket.fr/s-1/categories_2-huile',
-        'https://auroremarket.fr/s-1/categories_2-fruits_secs_et_fruits_seches',
-        'https://auroremarket.fr/s-1/categories_2-petit_dejeuner',
-        'https://auroremarket.fr/s-1/categories_2-boissons',
-        'https://auroremarket.fr/s-1/categories_2-plats_prepares',
-        'https://auroremarket.fr/s-1/categories_2-farine',
-        'https://auroremarket.fr/s-1/categories_2-compote',
-        'https://auroremarket.fr/s-1/categories_2-chocolat_et_cacao',
-        'https://auroremarket.fr/s-1/categories_2-epicerie_sucree',
-        'https://auroremarket.fr/s-1/categories_2-viandes_pate_et_poissons',
-        'https://auroremarket.fr/s-1/categories_2-pates_riz_graines_et_cereales',
-        'https://auroremarket.fr/s-1/categories_2-vins_rouges',
-        'https://auroremarket.fr/s-1/categories_2-aperitifs_et_digestifs',
-        'https://auroremarket.fr/s-1/categories_2-vins_blancs',
-        'https://auroremarket.fr/s-1/categories_2-boissons_petillantes',
-        'https://auroremarket.fr/s-1/categories_2-vins_roses',
-        'https://auroremarket.fr/s-1/categories_2-bieres',
-        'https://auroremarket.fr/s-1/categories_2-produits_menagers',
-        'https://auroremarket.fr/s-1/categories_2-thermos_gourde_et_qualite_de_l_eau',
-        'https://auroremarket.fr/s-1/categories_2-autre',
-        'https://auroremarket.fr/s-1/categories_2-papier_et_conservation_des_aliments',
-        'https://auroremarket.fr/s-1/categories_2-pique_nique_et_aperitif',
-        'https://auroremarket.fr/s-1/categories_2-contenants_pour_vrac',
-        'https://auroremarket.fr/s-1/categories_2-produits_pour_la_vaisselle',
-        'https://auroremarket.fr/s-1/categories_2-soin_du_linge',
-        'https://auroremarket.fr/s-1/categories_2-ambiance',
-        'https://auroremarket.fr/s-1/categories_2-manches_longues',
-        'https://auroremarket.fr/s-1/categories_2-manches_courtes',
-        'https://auroremarket.fr/s-1/categories_2-manches_longues_2',
-        'https://auroremarket.fr/s-1/categories_2-manches_courtes_2',
-        'https://auroremarket.fr/s-1/categories_2-insectes_et_nuisibles',
-        'https://auroremarket.fr/s-1/categories_2-materiel',
-        'https://auroremarket.fr/s-1/categories_2-graines_et_semences',
-        'https://auroremarket.fr/s-1/categories_2-materiel_scolaire_et_de_bureau',
-        'https://auroremarket.fr/s-1/categories_2-couches',
-        'https://auroremarket.fr/s-1/categories_2-hygiene_et_change',
-        'https://auroremarket.fr/s-1/categories_2-biberons_et_accessoires',
-        'https://auroremarket.fr/s-1/categories_2-biscuits_bebe',
-        'https://auroremarket.fr/s-1/categories_2-cereales_bebe',
-        'https://auroremarket.fr/s-1/categories_2-petits_repas_sucres',
-        'https://auroremarket.fr/s-1/categories_2-petits_repas_sales',
-        'https://auroremarket.fr/s-1/categories_2-bouillie',
-        'https://auroremarket.fr/s-1/categories_2-lait_infantile_brebis_et_chevre',
-        'https://auroremarket.fr/s-1/categories_2-lait_infantile_vegetal',
-        'https://auroremarket.fr/s-1/categories_2-lait_infantile'
+        'https://auroremarket.fr/s-1/en_stock-oui?p=1',
     ]
 
     def parse(self, response):
@@ -74,8 +15,9 @@ class SpiderSpider(scrapy.Spider):
             yield scrapy.Request(url, callback=self.individual_page)
 
         # Calling next page
-        next_page_url = response.xpath('//li[@class="pagination_next"]/a/@href').extract_first()
-        yield scrapy.Request(next_page_url, callback=self.parse)
+        for page in range(2, 80):
+            next_page_url = 'https://auroremarket.fr/s-1/en_stock-oui?p={}'.format(page)
+            yield scrapy.Request(url=next_page_url)
 
     def individual_page(self, response):
         base_price = response.xpath('//span[@class="am_old_price"]/text()').re_first('(\d+\,\d+) €')
